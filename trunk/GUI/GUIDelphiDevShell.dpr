d6c51b347594f760371d442e62d5adc848dcc06c
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@6575a5d4-7fd2-d1d0-f1c4-4a91c690f49d>
2013-08-18 03:44:08 +0000

    Updated Installer Added validation to GUI APP to only allow run one instance

--- trunk/GUI/GUIDelphiDevShell.dpr
+++ trunk/GUI/GUIDelphiDevShell.dpr
@@ -2,6 +2,7 @@ program GUIDelphiDevShell;
 
 uses
   Vcl.Forms,
+  Windows,
   uAbout in 'uAbout.pas' {FrmAbout},
   uCheckUpdate in 'uCheckUpdate.pas' {FrmCheckUpdate},
   uWinInet in 'uWinInet.pas',
@@ -13,7 +14,32 @@ uses
 
 {$R *.res}
 
+procedure OnlyOne;
+var
+    hWnd, hMutex : THandle;
+    lpName      : PWideChar;
 begin
+  lpName := PWideChar(Application.Title);
+  hMutex := CreateMutex (nil, FALSE, lpName );
+  if WaitForSingleObject (hMutex, 0) = wait_TimeOut then
+  begin
+     SetWindowText(Application.Handle,'');
+     hWnd := FindWindow(nil,lpName);
+     if hWnd<>0 then
+     begin
+        if IsIconic(hWnd) then ShowWindow(hWnd, SW_RESTORE);
+        BringWindowToTop(hWnd);
+        SetForegroundWindow(hWnd);
+     end;
+     Application.ShowMainForm := False;
+     Application.Terminate;
+     Halt(0);
+  end;
+end;
+
+
+begin
+  OnlyOne;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   TStyleManager.TrySetStyle('Amakrits');
