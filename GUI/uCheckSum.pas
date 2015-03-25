c83098fd6c2727bda89ce620a3f2eae1e9cbcc66
Rodrigo Ruz <rodrigo.ruz.v@gmail.com>
2015-03-25 18:38:18 -0400

    Copyright Updated
    Copyright Updated

--- GUI/uCheckSum.pas
+++ GUI/uCheckSum.pas
@@ -2,7 +2,7 @@
 //
 // Unit uCheckSum
 // unit for the Delphi Dev Shell Tools
-// http://code.google.com/p/delphi-dev-shell-tools/
+// https://github.com/RRUZ/delphi-dev-shell-tools
 //
 // The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
 // you may not use this file except in compliance with the License. You may obtain a copy of the
@@ -15,7 +15,7 @@
 // The Original Code is uCheckSum.pas.
 //
 // The Initial Developer of the Original Code is Rodrigo Ruz V.
-// Portions created by Rodrigo Ruz V. are Copyright (C) 2013-2014 Rodrigo Ruz V.
+// Portions created by Rodrigo Ruz V. are Copyright (C) 2013-2015 Rodrigo Ruz V.
 // All Rights Reserved.
 //
 //**************************************************************************************************
