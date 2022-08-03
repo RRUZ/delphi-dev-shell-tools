a9e345b833b3c4e955590a04cd979960a6db3cf9
Jim McKeeth <jim@mckeeth.org>
2022-08-03 13:46:47 -0600

    Updated for 11 Alexandria and added elevation
    All calls to regsvr32 now elevate to admin automatically
    Build also unregisters the DLL, even restarting explorer if necessary.

--- Build_x86Release.bat
+++ Build_x86Release.bat
@@ -1,4 +1,18 @@
+@echo off
+set dll=%CD%\Win32\Release\DelphiDevShellTools.dll
+if exist %dll% (
+	echo Unregistering
+	powershell -Command "start-process cmd \"/c regsvr32 %dll% /u \" -Verb RunAs"
+	del %dll%
+	pause
+)
+if exist %dll% (
+	echo DLL was locked by explorer so restarting explorer.
+	taskkill /F /IM explorer.exe
+	del %dll%
+	start explorer.exe
+)
 BRCC32 VersionInfo.rc
-call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
+call "C:\Program Files (x86)\Embarcadero\Studio\22.0\bin\rsvars.bat"
 msbuild.exe "DelphiDevShellTools.dproj" /target:clean;build /p:Platform=Win32 /p:config=release
 pause
